e3002cf65797400f141529b4bba3d8b1894bc03e
GUSTAVO FERRAZ LAZARINI <gustavo.flazarini@senacsp.edu.br>
2025-06-24 09:42:11 -0300

    atualizado1

--- Projet1/Projet1/Projet1/Aula.cs
+++ Projet1/Projet1/Projet1/Aula.cs
@@ -5,7 +5,7 @@ namespace JogR
     class Aula
     {
         public static void MostrarVariaveis()
-        
+
         {
 
             /* comentário de bloco
@@ -14,7 +14,7 @@ namespace JogR
             */
 
             // uso para inteiro 
-           // int pontos = 0;
+            // int pontos = 0;
             // int vida = 2000;
 
             //float velocidade = 22.5f; // número menor 
@@ -23,18 +23,34 @@ namespace JogR
 
             /* TIPO DE TEXTO */
 
-           // string titulo = "Afraid";
-           // palavras
+            // string titulo = "Afraid";
+            // palavras
 
             //Console.WriteLine(titulo);
 
 
-    //char letra; 
-    // letra única 
+            //char letra; 
+            // letra única 
 
             /* Booleano */
 
             //bool playing = false;
             // falso para não começar e verdadeiro para o contrário
 
+            /*
+
+            function isColor(data)
+            {
+
+
+
+                const result = {
+
+        red: "vermelho",
+        blue: "azul",
+        green: "verde"
+        };
+        return IAsyncResult[data] || "Não é uma cor";
+    }
+    console.log(isColor("red")); */
         } } }
